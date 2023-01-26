d89ddc5d80becf380e43c975ee188ab34aabdf51
leyden <fusionsoccer05@gmail.com>
2023-01-26 09:45:49 -0600

    added starter code. nothing more

---
+++ index.js
@@ -0,0 +1,1 @@
+// **Hint**: Using the 5 Day Weather Forecast API, you'll notice that you will need to pass in coordinates instead of just a city name. Using the OpenWeatherMap APIs, how could we retrieve geographical coordinates given a city name?
